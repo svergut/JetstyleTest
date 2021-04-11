d468bedbdb09213429533ef2f04d903bd04a0514
svergut <anunnamedanonymous@gmail.com>
2021-04-11 23:37:32 +0500

    pretty close to needed result

--- JetstyleTest/MainWindow.xaml.cs
+++ JetstyleTest/MainWindow.xaml.cs
@@ -4,6 +4,10 @@ using System.Windows;
 using System.Windows.Automation;
 using System.Diagnostics;
 using System.Speech.Synthesis;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections.ObjectModel;
 
 namespace JetstyleTest
 {
@@ -14,83 +18,124 @@ namespace JetstyleTest
     {
         private string appName = "notepad";
         delegate void NotepadLaunchHandler(Process process);
-        event NotepadLaunchHandler NotepadLaunched;
+        private AutomationElement focusedWindow;
+        public ObservableCollection<string> Items { get; set; }
 
         public MainWindow()
         {
-            InitializeComponent();
-
             AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
             {
                 Debug.WriteLine(eventArgs.Exception.ToString());
             };
 
-            StartListeningForNotepadLaunch(1000);
+            InitializeComponent();
+            DataContext = this;
+            Items = new ObservableCollection<string>();
+            MenuHoverHistoryList.ItemsSource = Items;
 
-            Automation.AddAutomationFocusChangedEventHandler((o, e) => {
-                Console.WriteLine((o as AutomationElement).Current.Name);
-                Console.WriteLine(e.EventId);
-            });
-
-            NotepadLaunched += (process) =>
-            {
-                var notepadProcess = Process.GetProcessesByName(appName).First();
-                var notepadWindow = AutomationElement.FromHandle(notepadProcess.Handle);
-
-                //Automation.AddAutomationEventHandler(AutomationElementIdentifiers.ToolTipOpenedEvent, notepadWindow, TreeScope.Element, (o, e) => {
-                //    Console.WriteLine(e.EventId);
-                //});
-
-                //fetch all children then compare
-
-                var speechSynthesizer = new SpeechSynthesizer();
-
-                Automation.AddAutomationFocusChangedEventHandler((o, e) => {
-                    Console.WriteLine((o as AutomationElement).Current.BoundingRectangle + " " + (o as AutomationElement).Current.Name);
-                    
-
-                    var currentProcess = Process.GetCurrentProcess();
-
-                   
-                    if (currentProcess.ProcessName == notepadProcess.ProcessName)
-                    {
-                        var element = o as AutomationElement;
-
-                        if (element != null)
-                        {
-                            speechSynthesizer.SpeakAsyncCancelAll();
-                            speechSynthesizer.Speak(new Prompt(element.Current.Name));
-                        }
-                    }
-
-                    
-                        
-                });
-            };
-
-            
-        }  
-        
-        private void StartListeningForNotepadLaunch(double interval)
-        {
-            var timer = new System.Timers.Timer(interval);
-            timer.Start();
-
-            timer.Elapsed += (i, a) =>
-            {
-                var processes = Process.GetProcessesByName(appName);
-
-                if (processes.Length > 0)
-                {
-                    var notepadProcess = processes.First();
-
-                    NotepadLaunched?.Invoke(notepadProcess);
-
-                    timer.Stop();
-                }
-            };
+            Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
         }
 
-    
+        private AutomationElement GetParentWindow(AutomationElement element)
+        {
+            var walker = TreeWalker.ControlViewWalker;
+            AutomationElement elementParent;
+            var node = element;
+
+            try 
+            {
+                if (node == AutomationElement.RootElement)
+                {
+                    return node;
+                }
+                while (true)
+                {
+                    elementParent = walker.GetParent(node);
+
+                    if (elementParent == null)
+                        return null;
+
+                    if (elementParent == AutomationElement.RootElement)
+                        break;
+
+                    node = elementParent;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            return node;
+        }
+
+        private void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
+        {
+            try
+            {
+                var focusedElement = sender as AutomationElement;
+                var parentWindow = GetParentWindow(focusedElement);
+
+                if (parentWindow != focusedWindow)
+                {
+                    Console.WriteLine("focus changed");
+                    focusedWindow = parentWindow;
+                }
+                       
+
+                Console.WriteLine(UIAelementIsRequiredProcessWindow(focusedWindow, appName));
+
+                if (UIAelementIsRequiredProcessWindow(focusedWindow, appName))
+                {                   
+                    App.Current.Dispatcher.Invoke(() => { Items.Add(focusedElement.Current.Name); });
+                }                    
+            }
+
+            catch (ElementNotAvailableException)
+            {
+            }
+        }
+
+        private bool UIAelementIsRequiredProcessWindow(AutomationElement uiaElement, string processName) 
+        {
+            var uiaElementIsRequiredProcess = false;
+            var processesWithGivenName = Process.GetProcessesByName(processName);
+
+            if (processesWithGivenName.Length > 0)
+            {
+                uiaElementIsRequiredProcess = true;
+            }
+
+            return uiaElementIsRequiredProcess;
+        }
+
+        private void AddFocusChangeListener(object element)
+        {
+            var automationElement = element as AutomationElement;
+
+            Automation.AddAutomationFocusChangedEventHandler(HandleAutomationFocusChange);
+        }
+        
+        private void HandleAutomationFocusChange(object o, AutomationFocusChangedEventArgs e)
+        {
+            var speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.Rate = 4;
+
+            try
+            {
+                var element = o as AutomationElement;
+
+                if (element != null)
+                {
+
+                    speechSynthesizer.SpeakAsyncCancelAll();
+                    speechSynthesizer.Speak(new Prompt(element.Current.Name));
+                }
+            }
+            catch (ElementNotAvailableException exception)
+            {
+                Console.WriteLine("Automation framework error has occured");
+            }
+        }
     }
 }
